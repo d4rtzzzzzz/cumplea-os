9b5bdb1b887df3b28b7278287cf7566be6530f1a
d4rtzzz <98066938+d4rtzzzzzz@users.noreply.github.com>
2025-11-14 22:31:31 -0800

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from "https://unpkg.com/three@0.152.2/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.152.2/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.152.2/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from './three/three.module.js';
+import { OrbitControls } from './three/OrbitControls.js';
+
 
 const canvas = document.getElementById("scene");
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
